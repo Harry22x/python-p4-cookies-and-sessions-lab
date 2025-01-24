5286817f99edd4dc3eedaf3e6e32aa2725b75a1c
Sitoh7 <ha.wakhule@gmail.com>
2025-01-24 13:23:14 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -22,13 +22,23 @@ def clear_session():
 
 @app.route('/articles')
 def index_articles():
-
-    pass
+    articles = [article.to_dict() for article in Article.query.all()]
+    response = make_response(jsonify(articles),200)
+    return response
+   
 
 @app.route('/articles/<int:id>')
 def show_article(id):
-
-    pass
+    session['page_views'] = session.get('page_views') or 0
+    session['page_views'] += 1
+    if session['page_views']<=3:
+    
+        article = Article.query.filter_by(id=id).first()
+        response = make_response(jsonify(article.to_dict()),200)
+        return response
+    else:
+        return {'message': 'Maximum pageview limit reached'}, 401
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
